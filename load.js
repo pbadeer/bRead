e7557b1f3444030e81279982c2ddad0d1eee515c
Preston Badeer <prrstn@gmail.com>
2011-11-19 20:25:14 -0600

    Book Autocomplete
    Working functionality for a book name autocomplete.

--- load.js
+++ load.js
@@ -1,6 +1,81 @@
-var output = 'me'; // The ID of the HTML element where the scripture html is dumped.
-var current_book = '1JN';
-var current_chap = 1;
+var books = new Array();
+books[0] = "";
+books[1] = "genesis";
+books[2] = "exodus";
+books[3] = "leviticus";
+books[4] = "numbers";
+books[5] = "deuteronomy";
+books[6] = "joshua";
+books[7] = "judges";
+books[8] = "ruth";
+books[9] = "1 samuel";
+books[10] = "2 samuel";
+books[11] = "1 kings";
+books[12] = "2 kings";
+books[13] = "1 chronicles";
+books[14] = "2 chronicles";
+books[15] = "ezra";
+books[16] = "nehemiah";
+books[17] = "esther";
+books[18] = "job";
+books[19] = "psalm";
+books[20] = "proverbs";
+books[21] = "ecclesiastes";
+books[22] = "song of solomon";
+books[23] = "isaiah";
+books[24] = "jeremiah";
+books[25] = "lamentations";
+books[26] = "ezekiel";
+books[27] = "daniel";
+books[28] = "hosea";
+books[29] = "joel";
+books[30] = "amos";
+books[31] = "obadiah";
+books[32] = "jonah";
+books[33] = "micah";
+books[34] = "nahum";
+books[35] = "habakkuk";
+books[36] = "zephaniah";
+books[37] = "haggai";
+books[38] = "zechariah";
+books[39] = "malachi";
+books[40] = "matthew";
+books[41] = "mark";
+books[42] = "luke";
+books[43] = "john";
+books[44] = "acts";
+books[45] = "romans";
+books[46] = "1 corinthians";
+books[47] = "2 corinthians";
+books[48] = "galatians";
+books[49] = "ephesians";
+books[50] = "philippians";
+books[51] = "colossians";
+books[52] = "1 thessalonians";
+books[53] = "2 thessalonians";
+books[54] = "1 timothy";
+books[55] = "2 timothy";
+books[56] = "titus";
+books[57] = "philemon";
+books[58] = "hebrews";
+books[59] = "james";
+books[60] = "1 peter";
+books[61] = "2 peter";
+books[62] = "1 john";
+books[63] = "2 john";
+books[64] = "3 john";
+books[65] = "jude";
+books[66] = "revelation";
+
+output = document.getElementById('pass');
+input = document.getElementById('ref');
+current_book = 'MAT';
+current_chap = 1;
+
+function clear()
+{
+  output.innerHTML = '';
+}
 
 function openXML(path)
 {
@@ -14,7 +89,11 @@ function openXML(path)
 
 function getHTML(book, n)
 {
-  if(!n) n = 1;
+  if(!n)
+  {
+    n = 1;
+    current_chap = 1;
+  }
 
   xml = openXML('bible/kjv/' + book + '/' + n + '.xml');
   xsl = openXML('chapter.xsl');
@@ -29,28 +108,36 @@ function getHTML(book, n)
 
 function loadBook(book)
 {
-  document.getElementById(output).appendChild( getHTML( book ) );
+  clear();
+  current_book = book;
+  output.appendChild( getHTML( book, 1 ) );
 }
 
 function loadChapter(n)
 {
-  document.getElementById(output).appendChild( getHTML( current_book, n ) );
+  clear();
+  current_chap = n;
+  output.appendChild( getHTML( current_book, n ) );
 }
 
 function loadRef()
 {
-  var ref = document.getElementById('ref').value;
-  loadBook(ref);
+  loadBook(input.value);
 }
 
 function prev()
 {
-  current_chap--;
-  loadChapter(current_chap);
+  loadChapter(current_chap - 1);
 }
 
 function next()
 {
-  current_chap++;
-  loadChapter(current_chap);
+  loadChapter(current_chap + 1);
+}
+
+
+function find(ref)
+{
+    for (var i=0; i<books.length; i++)
+      if (books[i].indexOf(ref.toLowerCase()) != -1) alert(books[i]);
 }
