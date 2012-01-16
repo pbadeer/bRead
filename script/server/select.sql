a80bec1c5bd066fc7136fe3cc7b5e302d55c51b6
Preston Badeer <prrstn@gmail.com>
2012-01-16 09:52:36 -0600

    SQL: Join reference to content, instead of vice-versa
    Previously joined content to content_reference, which caused empty
    results when references existed that had no content associated with
    them.

--- script/server/select.sql
+++ script/server/select.sql
@@ -8,8 +8,8 @@ SELECT
     content_reference.end_verse,
     content_type.content_type,
     content_privacy.content_privacy
-FROM content_reference
-LEFT JOIN content ON content.content_reference_id = content_reference.content_reference_id
+FROM content
+LEFT JOIN content_reference ON content_reference.content_reference_id = content.content_reference_id
 LEFT JOIN content_type ON content.content_type_id = content_type.content_type_id
 LEFT JOIN content_privacy ON content.content_privacy_id = content_privacy.content_privacy_id
 WHERE start_book_id = :book_id AND start_chapter = :chapter
