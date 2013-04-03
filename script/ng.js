b895d29bca68b3603ffe2914bb68a5b381e6b765
Preston Badeer <prrstn@gmail.com>
2013-04-03 17:01:45 -0500

    Successful JSON processing of XML data
    Angular is now getting data

--- script/ng.js
+++ script/ng.js
@@ -1,41 +1,18 @@
-function bible($scope) {
-	$scope.chapters = [
-		{
-			translation: "ABC",
-			book: "Genesis",
-			bookId: 1,
-			num: 1,
-			verses: [
-				{num: 1, content: "verse content"},
-				{num: 2, content: "verse content"},
-				{num: 3, content: "verse content"},
-				{num: 4, content: "verse content"},
-				{num: 5, content: "verse content"},
-				{num: 6, content: "verse content"},
-				{num: 8, content: "verse content"},
-				{num: 9, content: "verse content"},
-				{num: 10, content: "verse content"},
-				{num: 11, content: "verse content"},
-				{num: 12, content: "verse content"},
-				{num: 13, content: "verse content"},
-				{num: 14, content: "verse content"},
-				{num: 15, content: "verse content"},
-				{num: 16, content: "verse content"}
-			]
-		},
-		{
-			translation: "ABC",
-			book: "Genesis",
-			bookId: 1,
-			num: 2,
-			verses: [
-				{num: 1, content: "verse content"},
-				{num: 2, content: "verse content"}
-			]
-		}
-	];
+var book = 'test',
+    chapter = 1,
+    bookId = 1,
+    translation = 'NASB';
 
-	$scope.title = function() {
-
-	}
+function bible($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'data/' + translation + '/' + bookId + '/' + chapter + '.xml',
+        transformResponse: function(data) {
+            var json = x2js.xml_str2json(data);
+            return json;
+        }
+    }).success(function(data, status){
+        $scope.passage = data;
+        console.log(data)
+    });
 }
