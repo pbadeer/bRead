0dc20cf4e57e14d2a7b2970210e5112f763acd03
Preston Badeer <prrstn@gmail.com>
2013-04-03 11:54:31 -0500

    Templating foundations

---
+++ script/ng.js
@@ -0,0 +1,41 @@
+function bible($scope) {
+	$scope.chapters = [
+		{
+			translation: "ABC",
+			book: "Genesis",
+			bookId: 1,
+			num: 1,
+			verses: [
+				{num: 1, content: "verse content"},
+				{num: 2, content: "verse content"},
+				{num: 3, content: "verse content"},
+				{num: 4, content: "verse content"},
+				{num: 5, content: "verse content"},
+				{num: 6, content: "verse content"},
+				{num: 8, content: "verse content"},
+				{num: 9, content: "verse content"},
+				{num: 10, content: "verse content"},
+				{num: 11, content: "verse content"},
+				{num: 12, content: "verse content"},
+				{num: 13, content: "verse content"},
+				{num: 14, content: "verse content"},
+				{num: 15, content: "verse content"},
+				{num: 16, content: "verse content"}
+			]
+		},
+		{
+			translation: "ABC",
+			book: "Genesis",
+			bookId: 1,
+			num: 2,
+			verses: [
+				{num: 1, content: "verse content"},
+				{num: 2, content: "verse content"}
+			]
+		}
+	];
+
+	$scope.title = function() {
+
+	}
+}
