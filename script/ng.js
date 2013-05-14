ac53ba8b743f3d3f6c2e5d8018d5c18fd359b635
Preston Badeer <prrstn@gmail.com>
2013-05-14 14:51:10 -0500

    First working navigation!
    Angular v1! The chapter navigation is functioning. Next up, building
    the user content resource.

--- script/ng.js
+++ script/ng.js
@@ -3,27 +3,42 @@ function bible($scope, $http) {
     $scope.bookId = 1;
     $scope.translation = 'NASB';
 
+    update();
+
     $scope.title = function(){
         return Book[$scope.bookId].name + ' ' + $scope.chapter;
     }
 
     $scope.next = function() {
-        $scope.chapter++;
+        if($scope.chapter < Book[$scope.bookId].chapters)
+            $scope.chapter++;
+        else if($scope.chapter == Book[$scope.bookId].chapters && $scope.bookId != 66) {// doesn't account for diff book amounts
+            $scope.bookId++;
+            $scope.chapter = 1;
+        }
+        update();
     }
 
     $scope.previous = function() {
-        $scope.chapter--;
+        if($scope.chapter > 1)
+            $scope.chapter--;
+        else if($scope.chapter == 1 && $scope.bookId != 1) {
+            $scope.bookId--;
+            $scope.chapter = Book[$scope.bookId].chapters;
+        }
+        update();
     }
 
-    // convert to $resource (http://docs.angularjs.org/api/ngResource.$resource)
-    $http({
-        method: 'GET',
-        url: 'data/' + $scope.translation + '/' + $scope.bookId + '/' + $scope.chapter + '.xml',
-        transformResponse: function(data) {
-            var json = x2js.xml_str2json(data);
-            return json;
-        }
-    }).success(function(data, status){
-        $scope.passage = data;
-    });
+    function update(){
+        $http({
+            method: 'GET',
+            url: 'data/' + $scope.translation + '/' + $scope.bookId + '/' + $scope.chapter + '.xml',
+            transformResponse: function(data) {
+                var json = x2js.xml_str2json(data);
+                return json;
+            }
+        }).success(function(data, status){
+            $scope.passage = data;
+        });
+    }
 }
