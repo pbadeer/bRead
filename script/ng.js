e9e829d137877eba5b33bf714a3ed21ef57d002e
Preston Badeer <prrstn@gmail.com>
2013-05-14 16:05:43 -0500

    Bugfix, navigation
    Fixed bug caused by last navigation compatibility change. Also restored
    the update method to it's old use of the success method, which is
    syntactically more correct than populating data inside the
    transformResponse property.

--- script/ng.js
+++ script/ng.js
@@ -10,9 +10,10 @@ function bible($scope, $http) {
     }
 
     $scope.next = function() {
+        books = Object.keys(Book).length;
         if($scope.chapter < Book[$scope.bookId].chapters)
             $scope.chapter++;
-        else if($scope.chapter == Book[$scope.bookId].chapters && $scope.bookId < Book.length) {
+        else if($scope.chapter == Book[$scope.bookId].chapters && $scope.bookId != books) {
             $scope.bookId++;
             $scope.chapter = 1;
         }
@@ -33,10 +34,12 @@ function bible($scope, $http) {
         $http({
             method: 'GET',
             url: 'data/' + $scope.translation + '/' + $scope.bookId + '/' + $scope.chapter + '.xml',
-            transformResponse: function(data) {
-                var json = x2js.xml_str2json(data);
-                $scope.passage = json;
+            transformResponse: function(xml) {
+                var json = x2js.xml_str2json(xml);
+                return json;
             }
+        }).success(function(data){
+            $scope.passage = data;
         });
     }
 }
