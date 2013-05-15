64febeb6b2914116945f525be2749914faad8b05
Preston Badeer <prrstn@gmail.com>
2013-05-14 22:35:41 -0500

    First UserContent grab
    Successfully getting usercontent from the db, this requires sample data
    to test.

--- script/ng.js
+++ script/ng.js
@@ -2,14 +2,15 @@ angular.module('bread', ['ngResource']);
 
 function bible($scope, $http, $resource) {
     $scope.chapter = 1;
-    $scope.bookId = 1;
+    $scope.bookId = 40;
     $scope.translation = 'NASB';
     $scope.passage;
     $scope.userContent;
 
     update();
+    userContent();
 
-    $scope.title = function(){
+    $scope.title = function() {
         return Book[$scope.bookId].name + ' ' + $scope.chapter;
     }
 
@@ -23,6 +24,7 @@ function bible($scope, $http, $resource) {
             $scope.chapter = 1;
         }
         update();
+        userContent();
     }
 
     $scope.previous = function() {
@@ -33,9 +35,10 @@ function bible($scope, $http, $resource) {
             $scope.chapter = Book[$scope.bookId].chapters;
         }
         update();
+        userContent();
     }
 
-    function update(){
+    function update() {
         $http({
             method: 'GET',
             url: 'data/' + $scope.translation + '/' + $scope.bookId + '/' + $scope.chapter + '.xml',
@@ -45,50 +48,27 @@ function bible($scope, $http, $resource) {
             }
         }).success(function(data){
             $scope.passage = data;
-            $scope.userContent = UserContent.GetContent({book_id: $scope.bookId, chapter: $scope.chapter});
         });
     }
 
-    // http://stackoverflow.com/questions/13269882/angularjs-resource-restful-example
-    // testing requires data in db
-    var UserContent = $resource('script/server/ajax.php',
-        {
-            action: '@action'
-        },
-        {
-            GetContent: {
-                method: "GET",
-                params: {
-                    action: 'get',
-                    book_id: '',
-                    chapter: ''
-                }
-            },
-            CreateContent: {
-                method: "GET",
-                params: {
-                    action: 'new',
-                    privacy: '', //get types from db
-                    content_note: '', //text
-                    content_tag: '', //comma separated array
-                    start_book_id: '', //get all the rest of these from Data.form
-                    start_chapter: '',
-                    start_verse: '',
-                    start_index: '',
-                    end_book_id: '',
-                    end_chapter: '',
-                    end_verse: '',
-                    end_index: ''
-                }
-            },
-            DeleteContent: {
-                method: "GET",
-                params: {
-                    action: 'delete',
-                    id: '',
-                    type: ''
-                }
+    function userContent() {
+        $http({
+            method: 'GET',
+            url: 'script/server/ajax.php?action=get&book_id=' + $scope.bookId + '&chapter=' + $scope.chapter,
+            transformResponse: function(xml) {
+                var json = x2js.xml_str2json(xml);
+                return json;
             }
+        }).success(function(data){
+            $scope.userContent = data;
         });
+    }
 
+    function createUserContent() {
+
+    }
+
+    function deleteUserContent() {
+
+    }
 }
